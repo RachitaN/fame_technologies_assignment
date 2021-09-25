7d77dd4b704dba2223bb15f490fd2705ecccbc22
rachita <rachitan.is19@bmsce.ac.in>
2021-09-25 12:43:35 +0530

    sql file re-commit

--- fame_technologies_assignment.sql
+++ fame_technologies_assignment.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 25, 2021 at 09:08 AM
+-- Generation Time: Sep 25, 2021 at 09:10 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.8
 
